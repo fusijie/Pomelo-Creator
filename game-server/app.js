351942d7da5a3633d93798dde5494277d9839459
fusijie <fusijie@vip.qq.com>
2017-07-14 11:13:50 +0800

    update multi-server.

--- game-server/app.js
+++ game-server/app.js
@@ -1,4 +1,19 @@
 var pomelo = require('pomelo');
+var dispatcher = require('./app/util/dispatcher');
+
+// route definition for chat server
+var chatRoute = function(session, msg, app, cb) {
+  var chatServers = app.getServersByType('chat');
+
+	if(!chatServers || chatServers.length === 0) {
+		cb(new Error('can not find chat servers.'));
+		return;
+	}
+
+	var res = dispatcher.dispatch(session.get('rid'), chatServers);
+
+	cb(null, res.id);
+};
 
 /**
  * Init app for client.
@@ -19,8 +34,16 @@ app.configure('production|development', 'gate', function(){
 	app.set('connectorConfig',
 		{
 			connector : pomelo.connectors.hybridconnector,
-			useProtobuf : true
 		});
+});
+
+// app configure
+app.configure('production|development', function() {
+	// route configures
+	app.route('chat', chatRoute);
+
+	// filter configures
+	app.filter(pomelo.timeout());
 });
 
 // start app
